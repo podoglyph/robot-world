4bff9e07d39bb172b0c94d502623fd898a3a370a
Padraic O'Neill <pconeill@gmail.com [core]>
2017-05-02 18:07:16 -0600

    finished up to #delete

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -2,9 +2,47 @@ require_relative '../models/robot.rb'
 
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
+  set :method_override, true
+
+  attr_reader :robots, :robot
 
   get '/robots' do
+    @robots = Robot.all
     erb :index
   end
-end
 
+  get '/robots/new' do
+    erb :new_robot
+  end
+
+  post '/robots' do
+    robot = Robot.new(params[:robot])
+    robot.save
+    redirect '/robots'
+  end
+
+  get '/robots/:id' do
+    @robot = Robot.find(params[:id])
+    erb :show
+  end
+
+  get '/robots/:id/edit' do
+    @robot = Robot.find(params[:id])
+    erb :edit_robot
+  end
+
+  put '/robots/:id' do |id|
+    Robot.update(id.to_i, params[:robot])
+    redirect '/robots/#{id}'
+  end
+
+  delete '/robots/:id' do
+    Robot.delete(params[:id])
+    redirect '/robots'
+  end
+
+  not_found do
+    erb :error
+  end
+
+end
--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,8 +1,60 @@
 require 'sqlite3'
 
 class Robot
+  attr_reader :database, :id, :name, :city, :department, :state
+
   def initialize(robot_params)
+    @id = robot_params["id"] if robot_params["id"]
+    @name = robot_params["name"]
+    @city = robot_params["city"]
+    @state = robot_params["state"]
+    @department = robot_params["department"]
     @database = SQLite3::Database.new('db/robot_world_development.db')
     @database.results_as_hash = true
   end
+
+  def self.database
+    database = SQLite3::Database.new("db/robot_world_development.db")
+    database.results_as_hash = true
+    database
+  end
+
+  def self.all
+    robots = database.execute("SELECT * FROM robots;")
+
+    robots.map do |r|
+      Robot.new(r)
+    end
+  end
+
+  def self.find(id)
+    robot = database.execute("SELECT * FROM robots WHERE id=?", id).first
+    Robot.new(robot)
+  end
+
+  def self.edit(id, robot_params)
+    database.execute("UPDATE robots
+                      SET name = ?,
+                      city = ?,
+                      state = ?,
+                      department = ?
+                      WHERE id=?;",
+                      robot_params[:name],
+                      robot_params[:city],
+                      robot_params[:state],
+                      robot_params[:department],
+                      id)
+  end
+
+
+  def self.delete(id)
+    database.execute("DELETE FROM robots
+                      WHERE id=?;",
+                      id)
+  end
+
+  def save
+    @database.execute("INSERT INTO robots (name, city, state, department) VALUES (?,?,?,?);", @name, @city, @state, @department)
+  end
+
 end
